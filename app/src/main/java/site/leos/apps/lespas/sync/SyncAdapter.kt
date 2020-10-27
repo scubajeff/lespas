635762025921ed146000f9547e71c09987c5af89
Jeff <scubajeffrey@criptext.com>
2020-10-27 16:59:43 +0800

    switch to application context;

--- app/src/main/java/site/leos/apps/lespas/sync/SyncAdapter.kt
+++ app/src/main/java/site/leos/apps/lespas/sync/SyncAdapter.kt
@@ -5,7 +5,7 @@ import android.content.*
 import android.os.Bundle
 
 class SyncAdapter @JvmOverloads constructor(
-    context: Context, autoInitialize: Boolean, allowParallelSyncs: Boolean = false, val mContextResolver: ContentResolver = context.contentResolver
+    val context: Application, autoInitialize: Boolean, allowParallelSyncs: Boolean = false, val mContextResolver: ContentResolver = context.contentResolver
 ) : AbstractThreadedSyncAdapter(context, autoInitialize, allowParallelSyncs){
 
     override fun onPerformSync(account: Account, extras: Bundle, authority: String, provider: ContentProviderClient, syncResult: SyncResult) {
