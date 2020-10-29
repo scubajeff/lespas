6d08b4ef2a813619e6c3a165ca10f64981df9b68
Jeff <scubajeffrey@criptext.com>
2020-10-29 23:22:45 +0800

    support DAV sync with adding/updating/deleting on server side;

--- app/src/main/java/site/leos/apps/lespas/sync/SyncAdapter.kt
+++ app/src/main/java/site/leos/apps/lespas/sync/SyncAdapter.kt
@@ -2,6 +2,7 @@ package site.leos.apps.lespas.sync
 
 import android.accounts.Account
 import android.accounts.AccountManager
+import android.app.Application
 import android.content.AbstractThreadedSyncAdapter
 import android.content.ContentProviderClient
 import android.content.Context
@@ -10,6 +11,10 @@ import android.os.Bundle
 import android.util.Log
 import com.thegrizzlylabs.sardineandroid.impl.OkHttpSardine
 import site.leos.apps.lespas.R
+import site.leos.apps.lespas.album.Album
+import site.leos.apps.lespas.album.AlbumRepository
+import site.leos.apps.lespas.photo.Photo
+import site.leos.apps.lespas.photo.PhotoRepository
 import javax.xml.namespace.QName
 
 class SyncAdapter @JvmOverloads constructor(context: Context, autoInitialize: Boolean, allowParallelSyncs: Boolean = false
@@ -29,50 +34,121 @@ class SyncAdapter @JvmOverloads constructor(context: Context, autoInitialize: Bo
             }
 
             // Make sure lespas base directory is there
-            if (!sardine.exists(resourceRoot)) sardine.createDirectory(resourceRoot)
-
-            val ncProps = setOf(
-                QName(DAV_NS, GETETAG_LOCALPART, "D"),
-                QName(DAV_NS, GETLASTMODIFIED_LOCALPART, "D"),
-                QName(DAV_NS, GETCONTENTTYPE_LOCALPART, "D"),
-                QName(DAV_NS, GETRESOURCETYPE_LOCALPART, "D"),
-                QName(OWNCLOUD_NS, UNIQUE_ID_LOCALPART, "oc"),
-                QName(OWNCLOUD_NS, SHARETYPE_LOCALPART, "oc"),
-                QName(NEXTCLOUD_NS, HASPREVIEW_LOCALPART, "nc"),
-                QName(OWNCLOUD_NS, CHECKSUMS_LOCALPART, "oc"),
-                QName(OWNCLOUD_NS, SIZE_LOCALPART, "oc"),
-                QName(OWNCLOUD_NS, DATA_FINGERPRINT_LOCALPART, "oc")
-            )
-            sardine.list(resourceRoot, FOLDER_CONTENT_DEPTH, ncProps).forEach { album->
-                Log.e("=======", "${album.name} ${album.etag} ${album.isDirectory} ${album.contentType} ${album.path} ${album.modified} ${album.customProps}")
+            if (!sardine.exists(resourceRoot)) {
+                sardine.createDirectory(resourceRoot)
+                return
             }
 
+            // TODO: could be const
+            val ncProps = setOf(
+                QName(DAV_NS, DAV_GETETAG, "D"),
+                QName(DAV_NS, DAV_GETLASTMODIFIED, "D"),
+                QName(DAV_NS, DAV_GETCONTENTTYPE, "D"),
+                QName(DAV_NS, DAV_RESOURCETYPE, "D"),
+                QName(OC_NS, OC_UNIQUE_ID, "oc"),
+                QName(OC_NS, OC_SHARETYPE, "oc"),
+                QName(NC_NS, NC_HASPREVIEW, "nc"),
+                QName(OC_NS, OC_CHECKSUMS, "oc"),
+                QName(OC_NS, OC_SIZE, "oc"),
+                QName(OC_NS, OC_DATA_FINGERPRINT, "oc")
+            )
+
+            val albumRepository = AlbumRepository.getRepository(context as Application)     // SyncService pass applicationContext to us
+            val photoRepository = PhotoRepository.getRepository(context as Application)     // SyncService pass applicationContext to us
+
+            // Compare remote and local album list
+            val localAlbums = albumRepository.getSyncStatus()
+            val pendingUpdate: MutableList<Album> = mutableListOf()
+            sardine.list(resourceRoot, FOLDER_CONTENT_DEPTH, ncProps).drop(1).run {
+                val remoteAlbum = mutableListOf<String>()
+                forEach { album ->
+                    remoteAlbum.add(album.customProps[OC_UNIQUE_ID]!!)
+                    if (album.isDirectory) {
+                        if (localAlbums[album.customProps[OC_UNIQUE_ID]!!] != album.etag) {
+                            Log.e("=======", "album changed: ${album.name} r_etag:${album.etag} l_etag:${localAlbums[album.customProps[OC_UNIQUE_ID]!!]}")
+                            pendingUpdate.add(Album(album.customProps[OC_UNIQUE_ID]!!, album.name, null, null, "", 0, album.modified, Album.BY_DATE_TAKEN_ASC, album.etag, 0))
+                        }
+                    }
+                }
+                //Log.e("======", "changed albums:${pendingUpdate.size}")
+
+                // Delete those albums not exist on server
+                for (localAlbum in localAlbums) {
+                    if (!remoteAlbum.contains(localAlbum.key)) {
+                        Log.e("=======", "deleting album: ${localAlbum.key}")
+                        albumRepository.deleteByIdSync(localAlbum.key)
+                    }
+                }
+
+            }
+
+            // Sync each changed album
+            val pendingDownload = mutableListOf<Photo>()
+            val remotePhotos = mutableListOf<String>()
+            for(album in pendingUpdate) {
+                // Update album first, so that it's photos can be insert without FOREIGN KEY constraint failed
+                albumRepository.upsertSync(album)
+
+                var count = 0
+                val localPhotos = photoRepository.getSyncStatus(album.id)
+                sardine.list("$resourceRoot/${album.name}", FOLDER_CONTENT_DEPTH, ncProps).run {
+                    forEach { remotePhoto->
+                        if (remotePhoto.contentType.startsWith("image", true)) {
+                            // Accumulate remote photos list
+                            remotePhotos.add(remotePhoto.customProps[OC_UNIQUE_ID]!!)
+
+                            count++
+                            if (localPhotos[remotePhoto.customProps[OC_UNIQUE_ID]!!] != remotePhoto.etag) {
+                                Log.e("=======", "updating photo: ${remotePhoto.name} r_etag:${remotePhoto.etag} l_etag:${localPhotos[remotePhoto.customProps[OC_UNIQUE_ID]!!]}")
+                                pendingDownload.add(Photo(remotePhoto.customProps[OC_UNIQUE_ID]!!, album.id, remotePhoto.name, remotePhoto.etag, null, remotePhoto.modified, 0))
+                            }
+                        }
+                    }
+                    //Log.e("===========", "total:$count changed photos:${pendingDownload.size}")
+
+                    // Update photo
+                    for (photo in pendingDownload) photoRepository.upsertSync(photo)
+
+                    // Delete those photos not exist on server
+                    for (localPhoto in localPhotos) {
+                        if (!remotePhotos.contains(localPhoto.key)) {
+                            Log.e("=======", "deleting photo: ${localPhoto.key}")
+                            photoRepository.deleteByIdSync(localPhoto.key)
+                        }
+                    }
+
+                    // Recycle the list
+                    remotePhotos.clear()
+                    pendingDownload.clear()
+                }
+            }
 
         } catch (e:Exception) {
-            e.printStackTrace()
+            Log.e("=======", e.toString())
+            //e.printStackTrace()
         }
     }
 
     companion object {
         // PROPFIND properties namespace
         const val DAV_NS = "DAV:"
-        const val OWNCLOUD_NS = "http://owncloud.org/ns"
-        const val NEXTCLOUD_NS = "http://nextcloud.org/ns"
+        const val OC_NS = "http://owncloud.org/ns"
+        const val NC_NS = "http://nextcloud.org/ns"
 
         // OC and NC defined localpart
-        const val UNIQUE_ID_LOCALPART = "fileid"
-        const val SHARETYPE_LOCALPART = "share-types"
-        const val CHECKSUMS_LOCALPART = "checksums"
-        const val HASPREVIEW_LOCALPART = "has-preview"
-        const val SIZE_LOCALPART = "size"
-        const val DATA_FINGERPRINT_LOCALPART = "data-fingerprint"
+        const val OC_UNIQUE_ID = "fileid"
+        const val OC_SHARETYPE = "share-types"
+        const val OC_CHECKSUMS = "checksums"
+        const val NC_HASPREVIEW = "has-preview"
+        const val OC_SIZE = "size"
+        const val OC_DATA_FINGERPRINT = "data-fingerprint"
 
         // WebDAV defined localpart
-        const val GETETAG_LOCALPART = "getetag"
-        const val GETLASTMODIFIED_LOCALPART = "getlastmodified"
-        const val GETCONTENTTYPE_LOCALPART = "getcontenttype"
-        const val GETRESOURCETYPE_LOCALPART = "resourcetype"
-        const val GETCONTENTLENGTH_LOCALPART = "getcontentlength"
+        const val DAV_GETETAG = "getetag"
+        const val DAV_GETLASTMODIFIED = "getlastmodified"
+        const val DAV_GETCONTENTTYPE = "getcontenttype"
+        const val DAV_RESOURCETYPE = "resourcetype"
+        const val DAV_GETCONTENTLENGTH = "getcontentlength"
 
         const val JUST_FOLDER_DEPTH = 0
         const val FOLDER_CONTENT_DEPTH = 1
