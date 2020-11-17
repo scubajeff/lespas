beffd62611f1291fa127894c0f4303712fb7fa9d
Jeff <scubajeffrey@criptext.com>
2020-11-17 13:17:38 +0800

    added add new album function in ShareReceiverActivity;

--- app/src/main/java/site/leos/apps/lespas/sync/ShareReceiverActivity.kt
+++ app/src/main/java/site/leos/apps/lespas/sync/ShareReceiverActivity.kt
@@ -33,38 +33,40 @@ class ShareReceiverActivity: AppCompatActivity() {
 
         if (files.isNotEmpty()) {
             destinationModel.getDestination().observe (this, Observer { album->
-                if (album.id.isEmpty()) {
-                    finish()
-                } else {
-                    // Choose existing album
-                    acquiringModel = ViewModelProvider(this, AcquiringDialogFragment.AcquiringViewModelFactory(application, files)).get(AcquiringDialogFragment.AcquiringViewModel::class.java)
-                    acquiringModel.getProgress().observe(this, Observer { progress->
-                        if (progress == files.size) {
-                            // Files are under control, we can create sync action now
-                            val actions = mutableListOf<Action>()
-                            files.forEach {uri->
-                                contentResolver.query(uri, null, null, null, null)?.apply {
-                                    val columnIndex = getColumnIndex(OpenableColumns.DISPLAY_NAME)
-                                    moveToFirst()
-                                    actions.add(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, album.id, album.name, getString(columnIndex), System.currentTimeMillis(), 1))
-                                    close()
-                                }
+                // Acquire files
+                acquiringModel = ViewModelProvider(this, AcquiringDialogFragment.AcquiringViewModelFactory(application, files)).get(AcquiringDialogFragment.AcquiringViewModel::class.java)
+                acquiringModel.getProgress().observe(this, Observer { progress->
+                    if (progress == files.size) {
+                        // Files are under control, we can create sync action now
+                        val actions = mutableListOf<Action>()
+
+                        // Create new album first
+                        if (album.id.isEmpty()) actions.add(Action(null, Action.ACTION_ADD_DIRECTORY_ON_SERVER, album.id, album.name, "", System.currentTimeMillis(), 1))
+
+                        files.forEach {uri->
+                            contentResolver.query(uri, null, null, null, null)?.apply {
+                                val columnIndex = getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                                moveToFirst()
+                                actions.add(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, album.id, album.name, getString(columnIndex), System.currentTimeMillis(), 1))
+                                close()
                             }
-                            actionModel.addActions(actions)
-
-                            // Request sync immediately, since the viewmodel observing Action table might not be running at this moments
-                            ContentResolver.requestSync(AccountManager.get(this).accounts[0], getString(R.string.sync_authority), Bundle().apply {
-                                putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true)
-                                putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true)
-                            })
-
-                            finish()
-                            overridePendingTransition(0, 0)
                         }
-                    })
-                    if (supportFragmentManager.findFragmentByTag(TAG_ACQUIRING_DIALOG) == null)
-                        AcquiringDialogFragment.newInstance(files).show(supportFragmentManager, TAG_ACQUIRING_DIALOG)
-                }
+                        actionModel.addActions(actions)
+
+                        // Request sync immediately, since the viewmodel observing Action table might not be running at this moments
+                        ContentResolver.requestSync(AccountManager.get(this).accounts[0], getString(R.string.sync_authority), Bundle().apply {
+                            putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true)
+                            putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true)
+                        })
+
+                        // Done
+                        finish()
+                        overridePendingTransition(0, 0)
+                    }
+                })
+
+                if (supportFragmentManager.findFragmentByTag(TAG_ACQUIRING_DIALOG) == null)
+                    AcquiringDialogFragment.newInstance(files).show(supportFragmentManager, TAG_ACQUIRING_DIALOG)
             })
 
             if (supportFragmentManager.findFragmentByTag(TAG_DESTINATION_DIALOG) == null)
