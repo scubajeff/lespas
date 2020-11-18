a96b18f097fea15025362ffe1c7437b22fe30a32
Jeff <scubajeffrey@criptext.com>
2020-11-18 12:20:49 +0800

    missing theme;

--- app/src/main/java/site/leos/apps/lespas/ConfirmDialogFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/ConfirmDialogFragment.kt
@@ -2,8 +2,9 @@ package site.leos.apps.lespas
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class ConfirmDialogFragment() : DialogFragment() {
     private lateinit var onPositiveConfirmedListener: OnPositiveConfirmedListener
@@ -18,12 +19,19 @@ class ConfirmDialogFragment() : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
-        MaterialAlertDialogBuilder(requireContext())
+        AlertDialog.Builder(requireContext(), R.style.Theme_LesPas_Dialog)
             .setMessage(arguments?.getString(MESSAGE))
             .setPositiveButton(android.R.string.ok) {_, _-> onPositiveConfirmedListener.onPositiveConfirmed() }
             .setNegativeButton(android.R.string.cancel) {_, _->}
             .create()
 
+    override fun onResume() {
+        // Set dialog width to a fixed ration of screen width
+        val width = (resources.displayMetrics.widthPixels * resources.getInteger(R.integer.dialog_width_ratio) / 100)
+        dialog!!.window!!.setLayout(width, WindowManager.LayoutParams.WRAP_CONTENT)
+
+        super.onResume()
+    }
     companion object {
         const val MESSAGE = "MESSAGE"
         fun newInstance(message: String) = ConfirmDialogFragment().apply { arguments = Bundle().apply { putString(MESSAGE, message) } }
