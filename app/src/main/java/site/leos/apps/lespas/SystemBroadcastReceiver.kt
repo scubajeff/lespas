615e3b1566010d5d481c8e9afe4e0c89cb747b39
Jeff <scubajeffrey@criptext.com>
2021-01-08 12:11:10 +0800

    fixed periodic sync not turned on after reboot;

--- app/src/main/java/site/leos/apps/lespas/SystemBroadcastReceiver.kt
+++ app/src/main/java/site/leos/apps/lespas/SystemBroadcastReceiver.kt
@@ -4,8 +4,12 @@ import android.accounts.AccountManager
 import android.app.ActivityManager
 import android.app.admin.DevicePolicyManager
 import android.content.BroadcastReceiver
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import androidx.preference.PreferenceManager
+import site.leos.apps.lespas.sync.SyncAdapter
 
 class SystemBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -23,6 +27,17 @@ class SystemBroadcastReceiver : BroadcastReceiver() {
                     if ((intent.extras?.getString(AccountManager.KEY_ACCOUNT_TYPE, "") == context.getString(R.string.account_type_nc)) &&
                         (intent.extras?.getString(AccountManager.KEY_ACCOUNT_NAME, "") == accounts[0].name))
                         (context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).clearApplicationUserData()
+                }
+
+                Intent.ACTION_BOOT_COMPLETED -> {
+                    // Turn on periodic sync after bootup
+                    if (PreferenceManager.getDefaultSharedPreferences(context).getBoolean(context.getString(R.string.sync_pref_key), false))
+                        ContentResolver.addPeriodicSync(
+                            AccountManager.get(context).accounts[0],
+                            context.getString(R.string.sync_authority),
+                            Bundle().apply { putInt(SyncAdapter.ACTION, SyncAdapter.SYNC_REMOTE_CHANGES) },
+                            6 * 60 * 60
+                        )
                 }
             }
         }
