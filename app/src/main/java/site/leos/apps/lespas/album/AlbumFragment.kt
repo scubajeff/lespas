scubajeff/lespas

commit 4e85c0de3dad14a0f40098dd496f21c0b9cb10df

append timestamp suffix to local created/modified photo, so that filename is unique, avoid tedious file locking handling;

app/src/main/java/site/leos/apps/lespas/photo/Photo.kt
@@ -44,6 +44,9 @@ abstract class PhotoDao: BaseDao<Photo>() {
     @Query("UPDATE ${Photo.TABLE_NAME} SET name = :newName WHERE id = :id")
     abstract fun changeName(id: String, newName: String)
 
+    @Query("UPDATE ${Photo.TABLE_NAME} SET name = :newName WHERE albumId = :albumId AND name = :oldName")
+    abstract suspend fun changeName(albumId: String, oldName: String, newName: String)
+
     @Query("SELECT * FROM ${Photo.TABLE_NAME} WHERE albumId = :albumId ORDER BY dateTaken ASC")
     abstract fun getAlbumPhotos(albumId: String): List<Photo>
 
@@ -57,8 +60,8 @@ abstract class PhotoDao: BaseDao<Photo>() {
     @Query("UPDATE ${Photo.TABLE_NAME} SET albumId = :newId WHERE albumId = :oldId")
     abstract fun fixNewPhotosAlbumId(oldId: String, newId: String)
 
-    @Query("UPDATE ${Photo.TABLE_NAME} SET id = :newId, eTag = :eTag, lastModified = :lastModified WHERE id = :oldId")
-    abstract fun fixPhotoId(oldId: String, newId: String, eTag: String, lastModified: LocalDateTime)
+    @Query("UPDATE ${Photo.TABLE_NAME} SET id = :newId, name = :newName, eTag = :eTag, lastModified = :lastModified WHERE id = :oldId")
+    abstract fun fixPhoto(oldId: String, newId: String, newName: String, eTag: String, lastModified: LocalDateTime)
 
     @Query("SELECT albumId, name FROM ${Photo.TABLE_NAME}")
     abstract fun getAllPhotoNameMap(): List<AlbumPhotoName>

app/src/main/java/site/leos/apps/lespas/photo/PhotoRepository.kt
@@ -14,6 +14,7 @@ class PhotoRepository(application: Application) {
     fun getETagsMap(albumId: String): Map<String, String> = photoDao.getETagsMap(albumId).map { it.id to it.eTag }.toMap()
     fun getNamesMap(albumId: String): Map<String, String> = photoDao.getNamesMap(albumId).map { it.id to it.name }.toMap()
     fun changeName(photoId: String, newName: String) = photoDao.changeName(photoId, newName)
+    suspend fun changeName(albumId: String, oldName: String, newName: String) = photoDao.changeName(albumId, oldName, newName)
     suspend fun deletePhotos(photos: List<Photo>) { photoDao.delete(photos)}
     fun getAllPhotoIdsByAlbum(albumId: String): List<PhotoName> = photoDao.getNamesMap(albumId)
     fun deletePhotosByAlbum(albumId: String) = photoDao.deletePhotosByAlbum(albumId)
@@ -21,7 +22,7 @@ class PhotoRepository(application: Application) {
     suspend fun insert(photos: List<Photo>) { photoDao.insert(photos) }
     suspend fun insert(photo: Photo) { photoDao.insert(photo) }
     fun fixNewPhotosAlbumId(oldId: String, newId: String) { photoDao.fixNewPhotosAlbumId(oldId, newId) }
-    fun fixPhotoId(oldId: String, newId: String, eTag: String, lastModified: LocalDateTime) { photoDao.fixPhotoId(oldId, newId, eTag, lastModified) }
+    fun fixPhoto(oldId: String, newId: String, newName: String, eTag: String, lastModified: LocalDateTime) { photoDao.fixPhoto(oldId, newId, newName, eTag, lastModified) }
     fun getAllPhotoNameMap(): List<AlbumPhotoName> = photoDao.getAllPhotoNameMap()
     //suspend fun updatePhoto(oldId: String, newId: String, eTag: String, lastModifiedDate: LocalDateTime, width: Int, height: Int, mimeType: String) { photoDao.updatePhoto(oldId, newId, eTag, lastModifiedDate, width, height, mimeType) }
     suspend fun replacePhoto(oldPhoto: Photo, newPhoto: Photo) { photoDao.replacePhoto(oldPhoto, newPhoto)

app/src/main/java/site/leos/apps/lespas/photo/PhotoSlideFragment.kt
@@ -272,6 +272,7 @@ class PhotoSlideFragment : Fragment() {
 
                         // Add newPhoto, delete old photo locally
                         val newPhoto = with(snapseedFile.name) { Tools.getPhotoParams("$appRootFolder/$this", JPEG, this).copy(id = this, albumId = album.id, name = this) }
+                        originalItem = newPhoto
                         albumModel.replacePhoto(photo, newPhoto)
                         // Fix album cover Id if required
                         if (album.cover == photo.id)
@@ -286,7 +287,8 @@ class PhotoSlideFragment : Fragment() {
 
                         // Add newPhoto, delete old photo remotely
                         with(mutableListOf<Action>()) {
-                            add(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, album.id, album.name, newPhoto.mimeType, newPhoto.name, System.currentTimeMillis(), 1))
+                            // Pass photo mimeType in Action's folderId property
+                            add(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, newPhoto.mimeType, album.name, newPhoto.id, newPhoto.name, System.currentTimeMillis(), 1))
                             add(Action(null, Action.ACTION_DELETE_FILES_ON_SERVER, album.id, album.name, photo.id, photo.name, System.currentTimeMillis(), 1))
                             actionModel.addActions(this)
                         }
@@ -298,7 +300,7 @@ class PhotoSlideFragment : Fragment() {
                     // Copy Snapseed output
 
                     // Append timestamp suffix to make a unique filename
-                    val fileName = "${snapseedFile.name.substringBeforeLast('.')}_${System.currentTimeMillis()}.jpeg"
+                    val fileName = "${snapseedFile.name.substringBeforeLast('.')}_${System.currentTimeMillis()}.${snapseedFile.name.substringAfterLast('.')}"
 
                     try {
                         snapseedFile.inputStream().use { input ->
@@ -317,8 +319,8 @@ class PhotoSlideFragment : Fragment() {
                     // Create new photo
                     albumModel.addPhoto(Tools.getPhotoParams("$appRootFolder/$fileName", JPEG, fileName).copy(id = fileName, albumId = album.id, name = fileName))
 
-                    // Upload changes to server, mimetype passed in fileId property
-                    actionModel.addAction(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, album.id, album.name, JPEG, fileName, System.currentTimeMillis(), 1))
+                    // Upload changes to server, mimetype passed in folderId property
+                    actionModel.addAction(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, JPEG, album.name, fileName, fileName, System.currentTimeMillis(), 1))
                 }
 
                 // Repeat editing of same source will generate multiple files with sequential suffix, remove Snapseed output to avoid tedious filename parsing

app/src/main/java/site/leos/apps/lespas/album/AlbumDetailFragment.kt
@@ -457,7 +457,8 @@ class AlbumDetailFragment : Fragment(), ActionMode.Callback, ConfirmDialogFragme
 
                         // Add newPhoto, delete old photo remotely
                         with(mutableListOf<Action>()) {
-                            add(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, album.id, album.name, newPhoto.mimeType, newPhoto.name, System.currentTimeMillis(), 1))
+                            // Pass photo mimeType in Action's folderId property
+                            add(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, newPhoto.mimeType, album.name, newPhoto.id, newPhoto.name, System.currentTimeMillis(), 1))
                             add(Action(null, Action.ACTION_DELETE_FILES_ON_SERVER, album.id, album.name, sharedPhoto.id, sharedPhoto.name, System.currentTimeMillis(), 1))
                             actionModel.addActions(this)
                         }
@@ -466,7 +467,7 @@ class AlbumDetailFragment : Fragment(), ActionMode.Callback, ConfirmDialogFragme
                     // Copy Snapseed output
 
                     // Append timestamp suffix to make a unique filename
-                    val fileName = "${snapseedFile.name.substringBeforeLast('.')}_${System.currentTimeMillis()}.jpeg"
+                    val fileName = "${snapseedFile.name.substringBeforeLast('.')}_${System.currentTimeMillis()}.${snapseedFile.name.substringAfterLast('.')}"
 
                     try {
                         snapseedFile.inputStream().use { input ->
@@ -482,8 +483,8 @@ class AlbumDetailFragment : Fragment(), ActionMode.Callback, ConfirmDialogFragme
                     // Create new photo
                     albumModel.addPhoto(Tools.getPhotoParams("$appRootFolder/$fileName", PhotoSlideFragment.JPEG, fileName).copy(id = fileName, albumId = album.id, name = fileName))
 
-                    // Upload changes to server, mimetype passed in fileId property
-                    actionModel.addAction(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, album.id, album.name, PhotoSlideFragment.JPEG, fileName, System.currentTimeMillis(), 1))
+                    // Upload changes to server, mimetype passed in folderId property
+                    actionModel.addAction(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, PhotoSlideFragment.JPEG, album.name, fileName, fileName, System.currentTimeMillis(), 1))
                 }
 
                 // Repeat editing of same source will generate multiple files with sequential suffix, remove Snapseed output to avoid tedious filename parsing

app/src/main/java/site/leos/apps/lespas/sync/AcquiringDialogFragment.kt
@@ -152,6 +152,7 @@ class AcquiringDialogFragment: DialogFragment() {
 
         init {
             viewModelScope.launch(Dispatchers.IO) {
+                var fileId = ""
                 var fileName = ""
                 var mimeType = ""
                 val appRootFolder = "${application.filesDir}${application.getString(R.string.lespas_base_folder_name)}"
@@ -170,7 +171,8 @@ class AcquiringDialogFragment: DialogFragment() {
                     contentResolver.query(uri, null, null, null, null)?.apply {
                         val columnIndex = getColumnIndex(OpenableColumns.DISPLAY_NAME)
                         moveToFirst()
-                        fileName = getString(columnIndex)
+                        fileId = getString(columnIndex)
+                        fileName = "${fileId.substringBeforeLast('.')}_${System.currentTimeMillis()}.${fileId.substringAfterLast('.')}"
                         mimeType = contentResolver.getType(uri)?.let { Intent.normalizeMimeType(it) } ?: ""
                         close()
                     }
@@ -179,7 +181,7 @@ class AcquiringDialogFragment: DialogFragment() {
                     if (!(mimeType.startsWith("image/", true) || mimeType.startsWith("video/", true))) return@forEachIndexed
 
                     // Update progress in UI
-                    withContext(Dispatchers.Main) { setProgress(index, fileName) }
+                    withContext(Dispatchers.Main) { setProgress(index, fileId) }
 
                     // Copy the file to our private storage
                     try {
@@ -197,18 +199,21 @@ class AcquiringDialogFragment: DialogFragment() {
                     }
 
                     // If no photo with same name exists in album, create new photo
-                    if (!(allPhotoName.contains(AlbumPhotoName(album.id, fileName)))) {
-                        newPhotos.add(Tools.getPhotoParams("$appRootFolder/$fileName", mimeType, fileName).copy(id = fileName, albumId = album.id, name = fileName))
+                    if (!(allPhotoName.contains(AlbumPhotoName(album.id, fileId)))) {
+                        newPhotos.add(Tools.getPhotoParams("$appRootFolder/$fileName", mimeType, fileId).copy(id = fileId, albumId = album.id, name = fileName))
 
                         // Update album start and end dates accordingly
                         date = newPhotos.last().dateTaken
                         if (date < album.startDate) album.startDate = date
                         if (date > album.endDate) album.endDate = date
+                    } else {
+                        // Replacing old photo image, mark down new filename
+                        photoRepository.changeName(album.id, fileId, fileName)
                     }
 
-                    // Pass photo mimeType in Action's fileId property
+                    // Pass photo mimeType in Action's folderId property
                     // TODO if user accidentally add the same photo to album twice, file gets upload twice too. Gonna compare last modification date to avoid this.
-                    photoActions.add(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, album.id, album.name, mimeType, fileName, System.currentTimeMillis(), 1))
+                    photoActions.add(Action(null, Action.ACTION_ADD_FILES_ON_SERVER, mimeType, album.name, fileId, fileName, System.currentTimeMillis(), 1))
                 }
 
                 if (newPhotos.isEmpty()) withContext(Dispatchers.Main) { setProgress(NO_MEDIA_FILE_FOUND, "") }
@@ -233,7 +238,7 @@ class AcquiringDialogFragment: DialogFragment() {
                     albumRepository.upsert(album)
 
                     // By setting progress to more than 100%, signaling the calling fragment/activity
-                    withContext(Dispatchers.Main) { setProgress(uris.size, fileName) }
+                    withContext(Dispatchers.Main) { setProgress(uris.size, fileId) }
                 }
             }
         }

commit 4f2022686e21a0ef22375295d216853c00147c86

fixed various bugs of new photos not being uploaded yet, these bugs are introduced by commit 4e85c0de3dad14a0f40098dd496f21c0b9cb10df

app/src/main/java/site/leos/apps/lespas/album/AlbumFragment.kt
@@ -396,7 +396,7 @@ class AlbumFragment : Fragment(), ActionMode.Callback, ConfirmDialogFragment.OnR
             this.covers.apply {
                 clear()
                 albums.forEach { album ->
-                    this.add(Photo(album.cover, album.id, "", "", LocalDateTime.now(), LocalDateTime.now(), album.coverWidth, album.coverHeight, "", album.coverBaseline))
+                    this.add(Photo(album.cover, album.id, album.name, "", LocalDateTime.now(), LocalDateTime.now(), album.coverWidth, album.coverHeight, "", album.coverBaseline))
                 }
             }
             DiffUtil.calculateDiff(object : DiffUtil.Callback() {

app/src/main/java/site/leos/apps/lespas/helper/ImageLoaderViewModel.kt
@@ -15,6 +15,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import site.leos.apps.lespas.R
 import site.leos.apps.lespas.photo.Photo
+import site.leos.apps.lespas.photo.PhotoRepository
 import java.io.File
 import kotlin.math.min
 
@@ -24,6 +25,8 @@ class ImageLoaderViewModel(application: Application) : AndroidViewModel(applicat
     private val errorBitmap = getBitmapFromVector(application, R.drawable.ic_baseline_broken_image_24)
     private val placeholderBitmap = getBitmapFromVector(application, R.drawable.ic_baseline_placeholder_24)
     private val jobMap = HashMap<Int, Job>()
+
+    private val photoRepository = PhotoRepository(application)
 
     fun interface LoadCompleteListener{
         fun onLoadComplete()
@@ -41,11 +44,17 @@ class ImageLoaderViewModel(application: Application) : AndroidViewModel(applicat
 
     private fun decodeBitmap(photo: Photo, type: String): Bitmap? {
         var bitmap: Bitmap? = null
-        var fileName = "$rootPath/${photo.id}"
+        var fileName = "${rootPath}/${photo.id}"
 
         if (!(File(fileName).exists())) {
-            fileName = "$rootPath/${photo.name}"
-            if (!(File(fileName).exists())) return errorBitmap
+            fileName = "${rootPath}/${photo.name}"
+            if (!(File(fileName).exists())) {
+                if (type == TYPE_SMALL_COVER || type == TYPE_COVER) {
+                    fileName = "${rootPath}/${photoRepository.getPhotoName(photo.id)}"
+                    if (!File(fileName).exists()) return errorBitmap
+                }
+                else return errorBitmap
+            }
         }
 
         try {

app/src/main/java/site/leos/apps/lespas/photo/BottomControlsFragment.kt
@@ -133,21 +133,32 @@ class BottomControlsFragment : Fragment(), MainActivity.OnWindowFocusChangedList
             setOnClickListener {
                 hideHandler.post(hideSystemUI)
                 with(currentPhotoModel.getCurrentPhoto().value!!) {
-                    File("${requireActivity().filesDir}${getString(R.string.lespas_base_folder_name)}", id).copyTo(File(requireActivity().cacheDir, name), true, 4096)
-                    val uri = FileProvider.getUriForFile(requireContext(), getString(R.string.file_authority), File(requireActivity().cacheDir, name))
-                    val mimeType = this.mimeType
+                    val privateName: String
+                    val publicName: String
+                    if (eTag.isNotEmpty()) {
+                        privateName = id
+                        publicName = name
+                    } else {
+                        privateName = name
+                        publicName = id
+                    }
+                    try {
+                        File("${requireActivity().filesDir}${getString(R.string.lespas_base_folder_name)}", privateName).copyTo(File(requireActivity().cacheDir, publicName), true, 4096)
+                        val uri = FileProvider.getUriForFile(requireContext(), getString(R.string.file_authority), File(requireActivity().cacheDir, publicName))
+                        val mimeType = this.mimeType
 
-                    startActivity(
-                        Intent.createChooser(
-                            Intent().apply {
-                                action = Intent.ACTION_SEND
-                                type = mimeType
-                                putExtra(Intent.EXTRA_STREAM, uri)
-                                clipData = ClipData.newUri(requireActivity().contentResolver, "", uri)
-                                flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-                            }, null, PendingIntent.getBroadcast(context, 0, Intent(PhotoSlideFragment.CHOOSER_SPY_ACTION), PendingIntent.FLAG_UPDATE_CURRENT).intentSender
+                        startActivity(
+                            Intent.createChooser(
+                                Intent().apply {
+                                    action = Intent.ACTION_SEND
+                                    type = mimeType
+                                    putExtra(Intent.EXTRA_STREAM, uri)
+                                    clipData = ClipData.newUri(requireActivity().contentResolver, "", uri)
+                                    flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                                }, null, PendingIntent.getBroadcast(context, 0, Intent(PhotoSlideFragment.CHOOSER_SPY_ACTION), PendingIntent.FLAG_UPDATE_CURRENT).intentSender
+                            )
                         )
-                    )
+                    } catch(e: Exception) { e.printStackTrace() }
                 }
             }
         }
@@ -156,21 +167,32 @@ class BottomControlsFragment : Fragment(), MainActivity.OnWindowFocusChangedList
             setOnClickListener {
                 hideHandler.post(hideSystemUI)
                 with(currentPhotoModel.getCurrentPhoto().value!!) {
-                    File("${requireActivity().filesDir}${getString(R.string.lespas_base_folder_name)}", id).copyTo(File(requireActivity().cacheDir, name), true, 4096)
-                    val uri = FileProvider.getUriForFile(requireContext(), getString(R.string.file_authority), File(requireActivity().cacheDir, name))
-                    val mimeType = this.mimeType
+                    val privateName: String
+                    val publicName: String
+                    if (eTag.isNotEmpty()) {
+                        privateName = id
+                        publicName = name
+                    } else {
+                        privateName = name
+                        publicName = id
+                    }
+                    try {
+                        File("${requireActivity().filesDir}${getString(R.string.lespas_base_folder_name)}", privateName).copyTo(File(requireActivity().cacheDir, publicName), true, 4096)
+                        val uri = FileProvider.getUriForFile(requireContext(), getString(R.string.file_authority), File(requireActivity().cacheDir, publicName))
+                        val mimeType = this.mimeType
 
-                    startActivity(
-                        Intent.createChooser(
-                            Intent().apply {
-                                action = Intent.ACTION_ATTACH_DATA
-                                setDataAndType(uri, mimeType)
-                                putExtra("mimeType", mimeType)
-                                //clipData = ClipData.newUri(requireActivity().contentResolver, "", uri)
-                                flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-                            }, null
+                        startActivity(
+                            Intent.createChooser(
+                                Intent().apply {
+                                    action = Intent.ACTION_ATTACH_DATA
+                                    setDataAndType(uri, mimeType)
+                                    putExtra("mimeType", mimeType)
+                                    //clipData = ClipData.newUri(requireActivity().contentResolver, "", uri)
+                                    flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                                }, null
+                            )
                         )
-                    )
+                    } catch(e: Exception) { e.printStackTrace() }
                 }
             }
         }

app/src/main/java/site/leos/apps/lespas/photo/Photo.kt
@@ -76,4 +76,7 @@ abstract class PhotoDao: BaseDao<Photo>() {
         delete(oldPhoto)
         insert(newPhoto)
     }
+
+    @Query("SELECT name FROM ${Photo.TABLE_NAME} WHERE id = :id")
+    abstract fun getName(id: String): String
 }

app/src/main/java/site/leos/apps/lespas/photo/PhotoRepository.kt
@@ -29,4 +29,5 @@ class PhotoRepository(application: Application) {
         //photoDao.replacePhoto(oldPhotoId, newPhoto.id, newPhoto.name, newPhoto.albumId, newPhoto.dateTaken, newPhoto.lastModified, newPhoto.width, newPhoto.height, newPhoto.mimeType, newPhoto.shareId)
     }
     fun removePhoto(photo: Photo) { photoDao.deleteSync(photo) }
+    fun getPhotoName(id: String): String = photoDao.getName(id)
 }

app/src/main/java/site/leos/apps/lespas/photo/PhotoSlideFragment.kt
@@ -166,6 +166,15 @@ class PhotoSlideFragment : Fragment() {
                         withContext(Dispatchers.Main) { currentPhotoModel.setCurrentPhoto(nextPhoto, null) }
                         albumModel.removePhoto(it)
                         actionModel.addAction(Action(null, Action.ACTION_DELETE_FILES_ON_SERVER, album.id, album.name, it.id, it.name, System.currentTimeMillis(), 1))
+
+                        // Remove image file
+                        val rootPath = "${requireActivity().filesDir}${getString(R.string.lespas_base_folder_name)}"
+                        try {
+                            File(rootPath, it.id).delete()
+                        } catch(e: Exception) { e.printStackTrace() }
+                        try {
+                            File(rootPath, it.name).delete()
+                        } catch(e: Exception) { e.printStackTrace() }
                     }
                 }
             }