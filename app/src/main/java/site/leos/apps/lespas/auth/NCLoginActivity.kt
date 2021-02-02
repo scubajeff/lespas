3f53a1947f4d0274e8b8202b8acf10cdd369cd7d
Jeff <scubajeffrey@criptext.com>
2021-02-02 23:50:30 +0800

    added self signed certificate support;

--- app/src/main/java/site/leos/apps/lespas/auth/NCLoginActivity.kt
+++ app/src/main/java/site/leos/apps/lespas/auth/NCLoginActivity.kt
@@ -6,10 +6,12 @@ import android.accounts.AccountManager
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
 import android.graphics.drawable.Animatable
 import android.graphics.drawable.AnimationDrawable
 import android.graphics.drawable.Drawable
+import android.net.http.SslError
 import android.os.Build
 import android.os.Bundle
 import android.text.Editable
@@ -42,6 +44,8 @@ import java.net.URL
 import java.net.UnknownHostException
 import java.util.*
 import java.util.regex.Pattern
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLPeerUnverifiedException
 import kotlin.collections.HashMap
 
 class NCLoginActivity : AppCompatActivity() {
@@ -50,6 +54,7 @@ class NCLoginActivity : AppCompatActivity() {
     private lateinit var inputArea: TextInputLayout
     private lateinit var hostInputText: TextInputEditText
     private lateinit var loadingSpinner: Drawable
+    private var selfSigned = false
 
     @SuppressLint("SetJavaScriptEnabled")   // Nextcloud authentication page requires JavaScript enabled
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -158,6 +163,10 @@ class NCLoginActivity : AppCompatActivity() {
                         inputArea.endIconMode = TextInputLayout.END_ICON_NONE
                     }
                 }
+
+                override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+                    if (error?.primaryError == SslError.SSL_IDMISMATCH && selfSigned) handler?.proceed() else handler?.cancel()
+                }
             }
             settings.apply {
                 cacheMode = WebSettings.LOAD_NO_CACHE
@@ -233,6 +242,7 @@ class NCLoginActivity : AppCompatActivity() {
                     setUserData(account, getString(R.string.nc_userdata_server_port), url.port.toString())
                     setUserData(account, getString(R.string.nc_userdata_username), username)
                     setUserData(account, getString(R.string.nc_userdata_secret), Base64.encodeToString("$username:$token".encodeToByteArray(), Base64.NO_WRAP))
+                    setUserData(account, getString(R.string.nc_userdata_selfsigned), selfSigned.toString())
                 }
 
                 val result = Bundle().apply {
@@ -282,37 +292,63 @@ class NCLoginActivity : AppCompatActivity() {
                 endIconMode = TextInputLayout.END_ICON_CUSTOM
             }
 
-            CoroutineScope(Dispatchers.IO).launch(Dispatchers.IO) {
-                result = pingServer(hostUrl)
+            CoroutineScope(Dispatchers.Default).launch(Dispatchers.Main) {
+                result = pingServer(hostUrl, false)
 
-                withContext(Dispatchers.Main) {
-                    if (result == HttpURLConnection.HTTP_OK) {
+                when (result) {
+                    HttpURLConnection.HTTP_OK -> {
                         // If everything ok, start loading the nextcloud authentication page in webview
                         // the webview will reveal after page loaded
                         authWebpage.loadUrl("$hostUrl${getString(R.string.login_flow_endpoint)}", HashMap<String, String>().apply { put(NEXTCLOUD_OCSAPI_HEADER, "true") })
-                    } else {
-                        inputArea.endIconMode = TextInputLayout.END_ICON_NONE
-                        hostInputText.apply {
-                            isEnabled = true
-                            error = when(result) {
-                                999-> getString(R.string.network_error)
-                                1000-> getString(R.string.unknown_host)
-                                else-> getString(R.string.host_not_valid)
+                    }
+                    998 -> {
+                        AlertDialog.Builder(hostInputText.context)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setTitle(getString(R.string.verify_ssl_certificate_title))
+                            .setCancelable(false)
+                            .setMessage(getString(R.string.verify_ssl_certificate_message, hostUrl.substringAfterLast("://").substringBefore('/')))
+                            .setPositiveButton(R.string.accept_certificate) { _, _ ->
+                                CoroutineScope(Dispatchers.Default).launch(Dispatchers.Main) {
+                                    result = pingServer(hostUrl, true)
+
+                                    if (result == HttpURLConnection.HTTP_OK) {
+                                        selfSigned = true
+                                        authWebpage.loadUrl("$hostUrl${getString(R.string.login_flow_endpoint)}", HashMap<String, String>().apply { put(NEXTCLOUD_OCSAPI_HEADER, "true") })
+                                    } else showError(result)
+                                }
                             }
-                        }
+                            .setNegativeButton(android.R.string.cancel) { _, _ ->  showError(1001)}
+                            .create().show()
+                    }
+                    else -> {
+                        showError(result)
                     }
                 }
             }
         }
     }
 
+    private fun showError(errorCode: Int) {
+        inputArea.endIconMode = TextInputLayout.END_ICON_NONE
+        hostInputText.apply {
+            isEnabled = true
+            error = when(errorCode) {
+                999-> getString(R.string.network_error)
+                404, 1000-> getString(R.string.unknown_host)
+                1001-> getString(R.string.certificate_error)
+                else-> getString(R.string.host_not_valid)
+            }
+        }
+    }
+
     /* Use nextcloud server capabilities OCS endpoint to validate host */
     @Suppress("BlockingMethodInNonBlockingContext")
-    private suspend fun pingServer(serverUrl: String): Int {
+    private suspend fun pingServer(serverUrl: String, acceptSelfSign: Boolean): Int {
         return withContext(Dispatchers.IO) {
             try {
                 var response = 0
-                (URL("$serverUrl${getString(R.string.server_capabilities_endpoint)}").openConnection() as HttpURLConnection).apply {
+                (URL("$serverUrl${getString(R.string.server_capabilities_endpoint)}").openConnection() as HttpsURLConnection).apply {
+                    if (acceptSelfSign) setHostnameVerifier { _, _ -> true }
                     setRequestProperty(NEXTCLOUD_OCSAPI_HEADER, "true")
                     connectTimeout = 2000
                     readTimeout = 5000
@@ -335,6 +371,9 @@ class NCLoginActivity : AppCompatActivity() {
                     disconnect()
                 }
                 response
+            } catch (e: SSLPeerUnverifiedException) {
+                // This certificate is issued by user installed CA, let user decided whether to trust it or not
+                998
             } catch (e: UnknownHostException) {
                 e.printStackTrace()
                 1000
