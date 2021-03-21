2eee980e3b07be34ffe2e3e960249ac4beef8650
Jeff <scubajeffrey@criptext.com>
2021-03-21 22:45:37 +0800

    simplify constructor

--- app/src/main/java/site/leos/apps/lespas/settings/AboutPreference.kt
+++ app/src/main/java/site/leos/apps/lespas/settings/AboutPreference.kt
@@ -13,12 +13,7 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
 import site.leos.apps.lespas.R
 
-class AboutPreference: Preference {
-    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attributeSet, defStyleAttr, defStyleRes) {}
-    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int): super(context, attributeSet, defStyleAttr) {}
-    constructor(context: Context, attributeSet: AttributeSet): super(context, attributeSet) {}
-    constructor(context: Context): super(context) {}
-
+class AboutPreference (context: Context, attributeSet: AttributeSet): Preference(context, attributeSet) {
     override fun onBindViewHolder(holder: PreferenceViewHolder?) {
         super.onBindViewHolder(holder)
         layoutResource = R.layout.preference_about
