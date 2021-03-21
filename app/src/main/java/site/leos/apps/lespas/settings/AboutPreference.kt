24297dcae5edff10a602ce7924381d0f2f0f8488
Jeff <scubajeffrey@criptext.com>
2021-03-21 22:22:23 +0800

    animate the logo in setting page

--- app/src/main/java/site/leos/apps/lespas/settings/AboutPreference.kt
+++ app/src/main/java/site/leos/apps/lespas/settings/AboutPreference.kt
@@ -1,7 +1,14 @@
 package site.leos.apps.lespas.settings
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.doOnAttach
 import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
 import site.leos.apps.lespas.R
@@ -15,6 +22,33 @@ class AboutPreference: Preference {
     override fun onBindViewHolder(holder: PreferenceViewHolder?) {
         super.onBindViewHolder(holder)
         layoutResource = R.layout.preference_about
-        holder?.itemView?.isClickable = false
+        holder?.itemView?.let { itemView->
+            val versionText = itemView.findViewById<TextView>(R.id.version)
+            itemView.isClickable = false
+            itemView.doOnAttach {
+                itemView.findViewById<ImageView>(R.id.logo).apply {
+                    versionText.visibility = View.INVISIBLE
+                    alpha = 0.3f
+                    pivotX = 50f
+                    pivotY = 0f
+                    scaleX = 3f
+                    scaleY = 3f
+                    animate().alpha(1f).scaleX(1f).scaleY(1f).setDuration(800).setInterpolator(AccelerateDecelerateInterpolator()).setListener(object: AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            versionText.apply {
+                                alpha = 0.5f
+                                animate().alpha(1f).setDuration(300).setInterpolator(AccelerateDecelerateInterpolator()).setListener(object: AnimatorListenerAdapter() {
+                                    override fun onAnimationEnd(animation: Animator?) {
+                                        super.onAnimationEnd(animation)
+                                        visibility = View.VISIBLE
+                                    }
+                                })
+                            }
+                        }
+                    })
+                }
+            }
+        }
     }
 }
