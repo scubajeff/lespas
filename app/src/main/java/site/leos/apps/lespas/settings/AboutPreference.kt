2069253df0d84cca62a5e9b4023b95a6ec93d3db
Jeff <scubajeffrey@criptext.com>
2021-03-22 13:33:09 +0800

    remove layout clipping of logo;

--- app/src/main/java/site/leos/apps/lespas/settings/AboutPreference.kt
+++ app/src/main/java/site/leos/apps/lespas/settings/AboutPreference.kt
@@ -14,26 +14,29 @@ import androidx.preference.PreferenceViewHolder
 import site.leos.apps.lespas.R
 
 class AboutPreference (context: Context, attributeSet: AttributeSet): Preference(context, attributeSet) {
+    init {
+        layoutResource = R.layout.preference_about
+    }
+
     override fun onBindViewHolder(holder: PreferenceViewHolder?) {
         super.onBindViewHolder(holder)
-        layoutResource = R.layout.preference_about
         holder?.itemView?.let { itemView->
-            val versionText = itemView.findViewById<TextView>(R.id.version)
             itemView.isClickable = false
             itemView.doOnAttach {
+                val versionText = itemView.findViewById<TextView>(R.id.lespas_version)
                 itemView.findViewById<ImageView>(R.id.logo).apply {
                     versionText.visibility = View.INVISIBLE
                     alpha = 0.3f
-                    pivotX = 50f
-                    pivotY = 0f
-                    scaleX = 3f
-                    scaleY = 3f
-                    animate().alpha(1f).scaleX(1f).scaleY(1f).setDuration(800).setInterpolator(AccelerateDecelerateInterpolator()).setListener(object: AnimatorListenerAdapter() {
+                    pivotX = 70f
+                    pivotY = 50f
+                    scaleX = 4f
+                    scaleY = 4f
+                    animate().alpha(1f).scaleX(1f).scaleY(1f).setDuration(500).setInterpolator(AccelerateDecelerateInterpolator()).setListener(object: AnimatorListenerAdapter() {
                         override fun onAnimationEnd(animation: Animator?) {
                             super.onAnimationEnd(animation)
                             versionText.apply {
                                 alpha = 0.5f
-                                animate().alpha(1f).setDuration(300).setInterpolator(AccelerateDecelerateInterpolator()).setListener(object: AnimatorListenerAdapter() {
+                                animate().alpha(1f).setDuration(200).setInterpolator(AccelerateDecelerateInterpolator()).setListener(object: AnimatorListenerAdapter() {
                                     override fun onAnimationEnd(animation: Animator?) {
                                         super.onAnimationEnd(animation)
                                         visibility = View.VISIBLE
