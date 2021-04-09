09033aebc0120265338f5001c5a6d22ae75c551b
Jeff <scubajeffrey@criptext.com>
2021-04-09 21:39:57 +0800

    disable reenter transition when getting back to SearchResultFragment

--- app/src/main/java/site/leos/apps/lespas/search/SearchResultFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/search/SearchResultFragment.kt
@@ -61,7 +61,7 @@ class SearchResultFragment : Fragment() {
                         val album = albumModel.getThisAlbum(result.photo.albumId)[0]
                         withContext(Dispatchers.Main) {
                             exitTransition = MaterialElevationScale(false).apply { duration = resources.getInteger(android.R.integer.config_shortAnimTime).toLong() }
-                            reenterTransition = MaterialElevationScale(true).apply { duration = resources.getInteger(android.R.integer.config_shortAnimTime).toLong() }
+                            //reenterTransition = MaterialElevationScale(true).apply { duration = resources.getInteger(android.R.integer.config_shortAnimTime).toLong() }
                             parentFragmentManager.beginTransaction().setReorderingAllowed(true).addSharedElement(imageView, ViewCompat.getTransitionName(imageView)!!)
                                 .replace(R.id.container_root, AlbumDetailFragment.newInstance(album, result.photo.id), AlbumDetailFragment::class.java.canonicalName).addToBackStack(null).commit()
                         }
@@ -80,7 +80,7 @@ class SearchResultFragment : Fragment() {
                         duration = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
                         excludeTarget(R.id.stub, true)
                     }
-                    reenterTransition = MaterialElevationScale(true).apply { duration = resources.getInteger(android.R.integer.config_shortAnimTime).toLong() }
+                    //reenterTransition = MaterialElevationScale(true).apply { duration = resources.getInteger(android.R.integer.config_shortAnimTime).toLong() }
                     parentFragmentManager.beginTransaction().setReorderingAllowed(true).addSharedElement(imageView, ViewCompat.getTransitionName(imageView)!!)
                         .replace(R.id.container_root, CameraRollFragment.newInstance(result.photo.id), CameraRollFragment::class.java.canonicalName).addToBackStack(null).commit()
                 }
