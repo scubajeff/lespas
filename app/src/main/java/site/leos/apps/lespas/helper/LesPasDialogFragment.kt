b9b26c6f597d6167b6a2b4d1ef9aa3662ce74ed4
Jeff <scubajeffrey@criptext.com>
2021-04-29 22:21:43 +0800

    custom shape drawable as private class in LesPasDialogFragment

--- app/src/main/java/site/leos/apps/lespas/helper/LesPasDialogFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/helper/LesPasDialogFragment.kt
@@ -1,5 +1,7 @@
 package site.leos.apps.lespas.helper
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -9,6 +11,10 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.color.MaterialColors
+import com.google.android.material.shape.CornerTreatment
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
+import com.google.android.material.shape.ShapePath
 import site.leos.apps.lespas.R
 
 open class LesPasDialogFragment(private val layoutId: Int): DialogFragment() {
@@ -33,6 +39,37 @@ open class LesPasDialogFragment(private val layoutId: Int): DialogFragment() {
             }
             setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             setWindowAnimations(R.style.Theme_LesPas_Dialog_Animation)
+        }
+    }
+
+    private class DialogShapeDrawable: MaterialShapeDrawable() {
+        class ConcaveRoundedCornerTreatment : CornerTreatment() {
+
+            override fun getCornerPath(shapePath: ShapePath, angle: Float, interpolation: Float, radius: Float) {
+                val interpolatedRadius = radius * interpolation
+                shapePath.reset(0f, interpolatedRadius, ANGLE_LEFT, ANGLE_LEFT - angle)
+                shapePath.addArc(-interpolatedRadius, -interpolatedRadius, interpolatedRadius, interpolatedRadius, ANGLE_BOTTOM, -angle)
+            }
+
+            companion object {
+                const val ANGLE_LEFT = 180f
+                const val ANGLE_BOTTOM = 90f
+            }
+        }
+
+        companion object {
+            const val NO_STROKE = -1
+
+            @JvmStatic
+            fun newInstance(context: Context, strokeColor: Int) = Companion.newInstance(context, strokeColor, false)
+
+            @JvmStatic
+            fun newInstance(context: Context, strokeColor: Int, smallRadius: Boolean) = MaterialShapeDrawable(
+                ShapeAppearanceModel.builder().setAllCornerSizes(context.resources.getDimension(if (smallRadius) R.dimen.dialog_frame_radius_small else R.dimen.dialog_frame_radius)).setAllCorners(ConcaveRoundedCornerTreatment()).build()
+            ).apply {
+                fillColor = ColorStateList.valueOf(context.resources.getColor(R.color.color_background, null))
+                if (strokeColor != NO_STROKE) { setStroke(4.0f, strokeColor) }
+            }
         }
     }
 }
