568f604877c5858d7bcbb48700f1033184c2f9c6
Jeff <scubajeffrey@criptext.com>
2021-05-13 10:59:30 +0800

    default hide bottom control

--- app/src/main/java/site/leos/apps/lespas/photo/BottomControlsFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/photo/BottomControlsFragment.kt
@@ -49,7 +49,7 @@ class BottomControlsFragment : Fragment(), MainActivity.OnWindowFocusChangedList
     private lateinit var setAsButton: ImageButton
     private val currentPhotoModel: PhotoSlideFragment.CurrentPhotoViewModel by activityViewModels()
     private val uiToggle: PhotoSlideFragment.UIViewModel by activityViewModels()
-    private var ignore = true
+    //private var ignore = true
 
     private lateinit var removeOriginalBroadcastReceiver: RemoveOriginalBroadcastReceiver
 
@@ -84,12 +84,16 @@ class BottomControlsFragment : Fragment(), MainActivity.OnWindowFocusChangedList
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        uiToggle.status().observe(viewLifecycleOwner, { toggle() })
+/*
         uiToggle.status().observe(viewLifecycleOwner, {
             if (ignore) {
-                hideHandler.postDelayed(hideSystemUI, AUTO_HIDE_DELAY_MILLIS)
+                //hideHandler.postDelayed(hideSystemUI, AUTO_HIDE_DELAY_MILLIS)
+                hideHandler.post(hideSystemUI)
                 ignore = false
             } else toggle()
         })
+*/
         currentPhotoModel.getCurrentPhoto().observe(viewLifecycleOwner, {
             coverButton.isEnabled = !(Tools.isMediaPlayable(it.mimeType))
             setAsButton.isEnabled = !(Tools.isMediaPlayable(it.mimeType))
@@ -255,7 +259,7 @@ class BottomControlsFragment : Fragment(), MainActivity.OnWindowFocusChangedList
     }
 
     // Hide/Show controls, status bar, navigation bar
-    private var visible: Boolean = false
+    private var visible: Boolean = true
     private val hideHandler = Handler(Looper.getMainLooper())
 
     private fun toggle() {
