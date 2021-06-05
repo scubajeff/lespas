60c58c2d6d733af791cb894d919adc932a8ef432
Jeff <scubajeffrey@criptext.com>
2021-06-05 15:01:51 +0800

    use motion layout for AboutPreference

--- app/src/main/java/site/leos/apps/lespas/settings/AboutPreference.kt
+++ app/src/main/java/site/leos/apps/lespas/settings/AboutPreference.kt
@@ -1,60 +1,10 @@
 package site.leos.apps.lespas.settings
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
-import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.BounceInterpolator
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.view.doOnAttach
 import androidx.preference.Preference
-import androidx.preference.PreferenceViewHolder
 import site.leos.apps.lespas.R
 
 class AboutPreference (context: Context, attributeSet: AttributeSet): Preference(context, attributeSet) {
-    init {
-        layoutResource = R.layout.preference_about
-    }
-
-    override fun onBindViewHolder(holder: PreferenceViewHolder?) {
-        super.onBindViewHolder(holder)
-        holder?.itemView?.let { itemView->
-            itemView.isClickable = false
-            itemView.doOnAttach {
-                val versionText = itemView.findViewById<TextView>(R.id.lespas_version)
-                itemView.findViewById<ImageView>(R.id.logo).apply {
-                    versionText.visibility = View.INVISIBLE
-/*
-                    alpha = 0.3f
-                    pivotX = 70f
-                    pivotY = 50f
-                    scaleX = 4f
-                    scaleY = 4f
-                    animate().alpha(1f).scaleX(1f).scaleY(1f).setDuration(500).setInterpolator(AccelerateDecelerateInterpolator()).setListener(object: AnimatorListenerAdapter() {
-*/
-                    scaleX = 0.3f
-                    scaleY = 0.3f
-                    pivotX = 50f
-                    pivotY = 100f
-                    animate().scaleX(1f).scaleY(1f).setDuration(700).setInterpolator(BounceInterpolator()).setListener(object: AnimatorListenerAdapter() {
-                        override fun onAnimationEnd(animation: Animator?) {
-                            super.onAnimationEnd(animation)
-                            versionText.apply {
-                                alpha = 0.5f
-                                animate().alpha(1f).setDuration(50).setInterpolator(AccelerateDecelerateInterpolator()).setListener(object: AnimatorListenerAdapter() {
-                                    override fun onAnimationEnd(animation: Animator?) {
-                                        super.onAnimationEnd(animation)
-                                        visibility = View.VISIBLE
-                                    }
-                                })
-                            }
-                        }
-                    })
-                }
-            }
-        }
-    }
+    init { layoutResource = R.layout.preference_about }
 }
