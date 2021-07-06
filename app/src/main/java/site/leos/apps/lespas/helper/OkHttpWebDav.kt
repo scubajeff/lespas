26f7a3842c2ca5ea7a8ce43ec2fc1cf7674856d2
Jeff <scubajeffrey@criptext.com>
2021-07-06 10:19:54 +0800

    let called send absolute path;

--- app/src/main/java/site/leos/apps/lespas/helper/OkHttpWebDav.kt
+++ app/src/main/java/site/leos/apps/lespas/helper/OkHttpWebDav.kt
@@ -1,7 +1,6 @@
 package site.leos.apps.lespas.helper
 
 import android.content.ContentResolver
-import android.content.Context
 import android.net.Uri
 import android.os.Parcelable
 import android.util.Xml
@@ -19,14 +18,13 @@ import java.nio.charset.StandardCharsets
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-class OkHttpWebDav(context: Context, private val userId: String, password: String, serverAddress: String, selfSigned: Boolean, appBase: String?, userAgent: String?) {
-    private val serverBase = "${serverAddress}/remote.php/dav/files/${userId}${appBase.orEmpty()}"
+class OkHttpWebDav(private val userId: String, password: String, serverAddress: String, selfSigned: Boolean, cacheFolder: String, userAgent: String?) {
     private val chunkUploadBase = "${serverAddress}/remote.php/dav/uploads/${userId}"
     private val httpClient: OkHttpClient = OkHttpClient.Builder().apply {
         if (selfSigned) hostnameVerifier { _, _ -> true }
         addInterceptor { chain -> chain.proceed(chain.request().newBuilder().header("Authorization", Credentials.basic(userId, password, StandardCharsets.UTF_8)).build()) }
         addNetworkInterceptor { chain -> chain.proceed((chain.request().newBuilder().removeHeader("User-Agent").addHeader("User-Agent", userAgent ?: "OkHttpWebDav").build())) }
-        cache(Cache(File("${context.cacheDir}${appBase.orEmpty()}"), DISK_CACHE_SIZE))
+        cache(Cache(File(cacheFolder), DISK_CACHE_SIZE))
         addNetworkInterceptor { chain -> chain.proceed(chain.request()).newBuilder().removeHeader("Pragma").header("Cache-Control", "public, max-age=${MAX_AGE}").build() }
         retryOnConnectionFailure(true)
     }.build()
@@ -34,7 +32,7 @@ class OkHttpWebDav(context: Context, private val userId: String, password: Strin
     fun copy(source: String, dest: String) { copyOrMove(true, source, dest) }
 
     fun createFolder(folderName: String): String {
-        httpClient.newCall(Request.Builder().url(getResourceUrl(folderName)).method("MKCOL", null).build()).execute().use { response ->
+        httpClient.newCall(Request.Builder().url(folderName).method("MKCOL", null).build()).execute().use { response ->
             when {
                 response.isSuccessful -> return response.headers["oc-fileid"] ?: ""
                 response.code == 405 -> return ""
@@ -44,13 +42,13 @@ class OkHttpWebDav(context: Context, private val userId: String, password: Strin
     }
 
     fun delete(targetName: String) {
-        httpClient.newCall(Request.Builder().url(getResourceUrl(targetName)).delete().build()).execute().use { response->
+        httpClient.newCall(Request.Builder().url(targetName).delete().build()).execute().use { response->
             if (!response.isSuccessful) throw OkHttpWebDavException(response)
         }
     }
 
     fun download(source: String, dest: String, cacheControl: CacheControl?) {
-        val reqBuilder = Request.Builder().url(getResourceUrl(source))
+        val reqBuilder = Request.Builder().url(source)
         cacheControl?.let { reqBuilder.cacheControl(cacheControl) }
         httpClient.newCall(reqBuilder.get().build()).execute().use { response->
             if (response.isSuccessful) File(dest).sink(false).buffer().writeAll(response.body!!.source().buffer)
@@ -59,7 +57,7 @@ class OkHttpWebDav(context: Context, private val userId: String, password: Strin
     }
 
     fun getStream(source: String, cacheControl: CacheControl?): InputStream {
-        val reqBuilder = Request.Builder().url(getResourceUrl(source))
+        val reqBuilder = Request.Builder().url(source)
         cacheControl?.let { reqBuilder.cacheControl(cacheControl) }
         httpClient.newCall(reqBuilder.get().build()).execute().use { response ->
             if (response.isSuccessful) return response.body!!.byteStream()
@@ -68,7 +66,7 @@ class OkHttpWebDav(context: Context, private val userId: String, password: Strin
     }
 
     fun isExisted(targetName: String): Boolean {
-        httpClient.newCall(Request.Builder().url(getResourceUrl(targetName)).cacheControl(CacheControl.FORCE_NETWORK).method("PROPFIND", null).header("Depth", JUST_FOLDER_DEPTH).build()).execute().use { response ->
+        httpClient.newCall(Request.Builder().url(targetName).cacheControl(CacheControl.FORCE_NETWORK).method("PROPFIND", null).header("Depth", JUST_FOLDER_DEPTH).build()).execute().use { response ->
             if (response.isSuccessful) return true
             else throw OkHttpWebDavException(response)
         }
@@ -77,7 +75,7 @@ class OkHttpWebDav(context: Context, private val userId: String, password: Strin
     fun list(targetName: String, depth: String): List<DAVResource> {
         val result = mutableListOf<DAVResource>()
 
-        httpClient.newCall(Request.Builder().url(getResourceUrl(targetName)).cacheControl(CacheControl.FORCE_NETWORK).method("PROPFIND", PROPFIND_BODY.toRequestBody("text/xml".toMediaType())).header("Depth", depth).build()).execute().use { response->
+        httpClient.newCall(Request.Builder().url(targetName).cacheControl(CacheControl.FORCE_NETWORK).method("PROPFIND", PROPFIND_BODY.toRequestBody("text/xml".toMediaType())).header("Depth", depth).build()).execute().use { response->
             if (response.isSuccessful) {
                 val parser = Xml.newPullParser()
                 parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, true)
@@ -126,20 +124,20 @@ class OkHttpWebDav(context: Context, private val userId: String, password: Strin
     fun move(source: String, dest: String) { copyOrMove(false, source, dest) }
 
     fun upload(source: String, dest: String, mimeType: String) {
-        httpClient.newCall(Request.Builder().url(getResourceUrl(dest)).put(source.toRequestBody(mimeType.toMediaTypeOrNull())).build()).execute().use { response->
+        httpClient.newCall(Request.Builder().url(dest).put(source.toRequestBody(mimeType.toMediaTypeOrNull())).build()).execute().use { response->
             if(!response.isSuccessful) throw OkHttpWebDavException(response)
         }
     }
 
     fun upload(source: File, dest: String, mimeType: String) {
-        httpClient.newCall(Request.Builder().url(getResourceUrl(dest)).put(source.asRequestBody(mimeType.toMediaTypeOrNull())).build()).execute().use { response->
+        httpClient.newCall(Request.Builder().url(dest).put(source.asRequestBody(mimeType.toMediaTypeOrNull())).build()).execute().use { response->
             if(!response.isSuccessful) throw OkHttpWebDavException(response)
         }
     }
 
     fun upload(source: Uri, dest: String, mimeType: String, contentResolver: ContentResolver) {
         contentResolver.openInputStream(source)?.use { input->
-            httpClient.newCall(Request.Builder().url(getResourceUrl(dest)).put(streamRequestBody(input, mimeType.toMediaTypeOrNull(), -1L)).build()).execute().use { response->
+            httpClient.newCall(Request.Builder().url(dest).put(streamRequestBody(input, mimeType.toMediaTypeOrNull(), -1L)).build()).execute().use { response->
                 if(!response.isSuccessful) throw OkHttpWebDavException(response)
             }
         } ?: throw Exception("InputStream provider crashed")
@@ -177,7 +175,7 @@ class OkHttpWebDav(context: Context, private val userId: String, password: Strin
             }
 
             // Tell server to assembly chunks
-            httpClient.newCall(Request.Builder().url("${uploadFolder}/.file").method("MOVE", null).headers(Headers.Builder().add("DESTINATION", getResourceUrl(dest)).add("OVERWRITE", "T").build()).build()).execute().use { response->
+            httpClient.newCall(Request.Builder().url("${uploadFolder}/.file").method("MOVE", null).headers(Headers.Builder().add("DESTINATION", dest).add("OVERWRITE", "T").build()).build()).execute().use { response->
                 // Upload interrupted, delete uploaded chunks
                 try { httpClient.newCall(Request.Builder().url(uploadFolder).delete().build()).execute().use {} } catch (e: Exception) { e.printStackTrace() }
                 throw OkHttpWebDavException(response)
@@ -190,13 +188,11 @@ class OkHttpWebDav(context: Context, private val userId: String, password: Strin
     }
 
     private fun copyOrMove(copy: Boolean, source: String, dest: String) {
-        val hb = Headers.Builder().add("DESTINATION", getResourceUrl(dest)).add("OVERWRITE", "T")
-        httpClient.newCall(Request.Builder().url(getResourceUrl(source)).method(if (copy) "COPY" else "MOVE", null).headers(hb.build()).build()).execute().use { response->
+        val hb = Headers.Builder().add("DESTINATION", dest).add("OVERWRITE", "T")
+        httpClient.newCall(Request.Builder().url(source).method(if (copy) "COPY" else "MOVE", null).headers(hb.build()).build()).execute().use { response->
             if (!response.isSuccessful) throw OkHttpWebDavException(response)
         }
     }
-
-    private fun getResourceUrl(targetName: String): String = "${serverBase}/${Uri.encode(targetName)}"
 
     private fun streamRequestBody(input: InputStream, mediaType: MediaType?, size: Long): RequestBody {
         return object : RequestBody() {
@@ -220,7 +216,6 @@ class OkHttpWebDav(context: Context, private val userId: String, password: Strin
         private const val DISK_CACHE_SIZE = 300L * 1024L * 1024L    // 300MB
         private const val MAX_AGE = "864000"        // 10 days
 
-        private const val UPLOAD_INTERRUPTED = "Trunk upload interrupted."
         const val CHUNK_SIZE = 10L * 1024L * 1024L  // Default chunk upload size is 10MB
 
         // PROPFIND depth
