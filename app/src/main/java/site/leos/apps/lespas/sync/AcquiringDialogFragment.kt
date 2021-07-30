f36b4c182de4f3db5de61f2362e77e01dd2cf358
Jeff <scubajeffrey@criptext.com>
2021-07-30 10:33:44 +0800

    fixed files acquisition hang when there are media already existed in the designated album

--- app/src/main/java/site/leos/apps/lespas/sync/AcquiringDialogFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/sync/AcquiringDialogFragment.kt
@@ -252,9 +252,8 @@ class AcquiringDialogFragment: LesPasDialogFragment(R.layout.fragment_acquiring_
                         }
                     } else {
                         // TODO show special error message when there are just some duplicate in uris
-                        //photoRepository.changeName(album.id, fileId, fileName)
                         if (uris.size == 1) setProgress(SAME_FILE_EXISTED, "")
-                        return@launch
+                        else setProgress(index, fileId)
                     }
                 }
 
