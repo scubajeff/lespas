c633bd38eebaee98744f1314b07dc444cbf13783
Jeff <scubajeffrey@criptext.com>
2022-04-25 21:18:00 +0800

    prevent launching Gallery if not logged in

--- app/src/main/java/site/leos/apps/lespas/cameraroll/CameraRollActivity.kt
+++ app/src/main/java/site/leos/apps/lespas/cameraroll/CameraRollActivity.kt
@@ -29,43 +29,44 @@ class CameraRollActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Make sure photo's folder, temporary cache folder created
-        lifecycleScope.launch(Dispatchers.IO) {
-            // Make sure photo's folder created
-            try {
-                File(Tools.getLocalRoot(applicationContext)).mkdir()
-            } catch (e: Exception) {}
-        }
+        AccountManager.get(this).getAccountsByType(getString(R.string.account_type_nc)).let { accounts ->
+            if (accounts.isNotEmpty()) {
+                // Make sure photo's folder, temporary cache folder created
+                lifecycleScope.launch(Dispatchers.IO) {
+                    // Make sure photo's folder created
+                    try { File(Tools.getLocalRoot(applicationContext)).mkdir() } catch (e: Exception) {}
+                }
 
-        if (savedInstanceState == null) {
-            // Sync with server at startup
-            AccountManager.get(this).getAccountsByType(getString(R.string.account_type_nc)).let { accounts ->
-                if (accounts.isNotEmpty()) {
+                if (savedInstanceState == null) {
+                    // Sync with server at startup
                     ContentResolver.requestSync(accounts[0], getString(R.string.sync_authority), Bundle().apply {
                         putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true)
                         //putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true)
                         putInt(SyncAdapter.ACTION, SyncAdapter.SYNC_BOTH_WAY)
                     })
+
+                    if (!PreferenceManager.getDefaultSharedPreferences(this).getBoolean(SettingsFragment.KEY_STORAGE_LOCATION, true) && (getSystemService(Context.STORAGE_SERVICE) as StorageManager).storageVolumes[1].state != Environment.MEDIA_MOUNTED) {
+                        // We need external SD mounted writable
+                        if (supportFragmentManager.findFragmentByTag(MainActivity.CONFIRM_REQUIRE_SD_DIALOG) == null) ConfirmDialogFragment.newInstance(getString(R.string.sd_card_not_ready), null, false, MainActivity.CONFIRM_REQUIRE_SD_DIALOG)
+                            .show(supportFragmentManager, MainActivity.CONFIRM_REQUIRE_SD_DIALOG)
+                    } else {
+                        if (intent.action == Intent.ACTION_MAIN) supportFragmentManager.beginTransaction().add(R.id.container_root, CameraRollFragment.newInstance(), CameraRollFragment.TAG_FROM_CAMERAROLL_ACTIVITY).commit()
+                        else intent.data?.apply { supportFragmentManager.beginTransaction().add(R.id.container_root, CameraRollFragment.newInstance(this), CameraRollFragment.TAG_FROM_CAMERAROLL_ACTIVITY).commit() } ?: run { finish() }
+                    }
                 }
-            }
 
-            if (!PreferenceManager.getDefaultSharedPreferences(this).getBoolean(SettingsFragment.KEY_STORAGE_LOCATION, true) && (getSystemService(Context.STORAGE_SERVICE) as StorageManager).storageVolumes[1].state != Environment.MEDIA_MOUNTED) {
-                // We need external SD mounted writable
-                if (supportFragmentManager.findFragmentByTag(MainActivity.CONFIRM_REQUIRE_SD_DIALOG) == null) ConfirmDialogFragment.newInstance(getString(R.string.sd_card_not_ready), null, false, MainActivity.CONFIRM_REQUIRE_SD_DIALOG)
-                    .show(supportFragmentManager, MainActivity.CONFIRM_REQUIRE_SD_DIALOG)
+                supportFragmentManager.setFragmentResultListener(MainActivity.ACTIVITY_DIALOG_REQUEST_KEY, this) { key, bundle ->
+                    if (key == MainActivity.ACTIVITY_DIALOG_REQUEST_KEY &&
+                        bundle.getBoolean(ConfirmDialogFragment.CONFIRM_DIALOG_REQUEST_KEY, false) &&
+                        bundle.getString(ConfirmDialogFragment.INDIVIDUAL_REQUEST_KEY, "") == MainActivity.CONFIRM_REQUIRE_SD_DIALOG
+                    ) finish()
+                }
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
             } else {
-                if (intent.action == Intent.ACTION_MAIN) supportFragmentManager.beginTransaction().add(R.id.container_root, CameraRollFragment.newInstance(), CameraRollFragment.TAG_FROM_CAMERAROLL_ACTIVITY).commit()
-                else intent.data?.apply {supportFragmentManager.beginTransaction().add(R.id.container_root, CameraRollFragment.newInstance(this), CameraRollFragment.TAG_FROM_CAMERAROLL_ACTIVITY).commit()} ?: run { finish() }
+                finish()
+                return
             }
         }
-
-        supportFragmentManager.setFragmentResultListener(MainActivity.ACTIVITY_DIALOG_REQUEST_KEY, this) { key, bundle ->
-            if (key == MainActivity.ACTIVITY_DIALOG_REQUEST_KEY &&
-                bundle.getBoolean(ConfirmDialogFragment.CONFIRM_DIALOG_REQUEST_KEY, false) &&
-                bundle.getString(ConfirmDialogFragment.INDIVIDUAL_REQUEST_KEY, "") == MainActivity.CONFIRM_REQUIRE_SD_DIALOG
-            ) finish()
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
     }
 }
