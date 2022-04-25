166c31c274fcef722a2eac0192b11cdd2dda12b9
Jeff <scubajeffrey@criptext.com>
2022-04-25 21:27:51 +0800

    fixed Gallery launcher not being removed after account logout

--- app/src/main/java/site/leos/apps/lespas/settings/SettingsFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/settings/SettingsFragment.kt
@@ -142,9 +142,9 @@ class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedP
             if (bundle.getBoolean(ConfirmDialogFragment.CONFIRM_DIALOG_REQUEST_KEY, false)) {
                 when(bundle.getString(ConfirmDialogFragment.INDIVIDUAL_REQUEST_KEY, "")) {
                     LOGOUT_CONFIRM_DIALOG -> {
+                        requireActivity().packageManager.setComponentEnabledSetting(ComponentName(BuildConfig.APPLICATION_ID, "${BuildConfig.APPLICATION_ID}.Gallery"), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP)
                         AccountManager.get(context).apply { removeAccountExplicitly(getAccountsByType(getString(R.string.account_type_nc))[0]) }
                         (requireContext().getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).clearApplicationUserData()
-                        requireActivity().packageManager.setComponentEnabledSetting(ComponentName(BuildConfig.APPLICATION_ID, "${BuildConfig.APPLICATION_ID}.Gallery"), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP)
                         requireActivity().finish()
                     }
                     SNAPSEED_PERMISSION_RATIONALE_REQUEST_DIALOG -> {
