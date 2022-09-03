2894a1a183c58bb5e42171ed28cd35f7e5b01cbb
Jeff <scubajeffrey@criptext.com>
2022-09-03 14:27:43 +0800

    Revert "fixed local sync action status report"
    This reverts commit 95c3fdf6f453a41e486be3ca1206c87fda5a69bc.

--- app/src/main/java/site/leos/apps/lespas/settings/SyncStatusDialogFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/settings/SyncStatusDialogFragment.kt
@@ -23,7 +23,6 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.text.format.DateUtils
-import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -166,7 +165,6 @@ class SyncStatusDialogFragment: LesPasDialogFragment(R.layout.fragment_sync_stat
                 // action is String array of: actionId|folderId|folderName|fileId|fileName|date
                 if (action.isNotEmpty()) {
                     val actionId = action[0].toInt()
-                    Log.e(">>>>>>>>", "getCurrentActionSummary: $action")
 
                     // Disable re-sync when syncing
                     reSyncButton.isEnabled = actionId >= Action.SYNC_RESULT_FINISHED
