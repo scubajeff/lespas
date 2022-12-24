43108d4abd0f10f15c641268387c50ae9c10aca4
Jeff <scubajeffrey@criptext.com>
2022-12-24 15:32:07 +0800

    Revert "fixed retrieving wrong timezone when adding photos"
    This reverts commit 1e4a7eb95c8e23cf2b0d4373266b2a059c05590f.

--- app/src/main/java/site/leos/apps/lespas/helper/Tools.kt
+++ app/src/main/java/site/leos/apps/lespas/helper/Tools.kt
@@ -132,7 +132,7 @@ object Tools {
                         bearing = getBearing(exif)
 
                         // Taken date
-                        getImageTakenDate(exif, true)?.let { dateTaken = it }
+                        getImageTakenDate(exif)?.let { dateTaken = it }
 /*
                         if (updateCreationDate) {
                             exif.setDateTime(dateTaken.toInstant(OffsetTime.now().offset).toEpochMilli())
