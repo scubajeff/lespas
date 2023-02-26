1c4abf7ab519a6070dee918bc218d362d8b4d1ad
Jeff <scubajeffrey@criptext.com>
2023-02-26 12:26:42 +0800

    Revert "fixed bug when album's very first or very last photo got date changed by renaming with a timestamp pattern name, the album's start/end date didn't changed accordingly"
    This reverts commit 2aa67b25cf4c7e4a348d3e2e754168a76f940fca.

--- app/src/main/java/site/leos/apps/lespas/sync/SyncAdapter.kt
+++ app/src/main/java/site/leos/apps/lespas/sync/SyncAdapter.kt
@@ -1102,7 +1102,6 @@ class SyncAdapter @JvmOverloads constructor(private val application: Application
                 //val metaFileName = "${changedAlbum.id}.json"
                 val bgmFileName = "${changedAlbum.id}${BGMDialogFragment.BGM_FILE_SUFFIX}"
                 var contentModifiedTime = LocalDateTime.MIN
-                var updateAlbumPeriodNeeded = false
 
                 // Create changePhotos list
                 //Log.e(TAG, "syncing remote album ${changedAlbum.name}")
@@ -1159,11 +1158,7 @@ class SyncAdapter @JvmOverloads constructor(private val application: Application
                                 contentMetaUpdatedNeeded.add(changedAlbum.name)
 
                                 // Parsing new filename for dataTaken string of yyyyMMddHHmmss or yyyyMMdd_HHmmss
-                                Tools.parseDateFromFileName(remotePhoto.name)?.let {
-                                    photoRepository.updateDateTaken(remotePhotoId, it)
-                                    // Might need to update album start/end time, set this flag because file renamed won't be added to chagnedPhotos list
-                                    updateAlbumPeriodNeeded = true
-                                }
+                                Tools.parseDateFromFileName(remotePhoto.name)?.let { photoRepository.updateDateTaken(remotePhotoId, it) }
 
                                 // If album's cover's filename changed on server
                                 if (remotePhotoId == changedAlbum.cover) {
@@ -1506,10 +1501,6 @@ class SyncAdapter @JvmOverloads constructor(private val application: Application
                     }
 
                     // Maintain album start and end date
-                    updateAlbumPeriodNeeded = true
-                }
-
-                if (updateAlbumPeriodNeeded) {
                     with(photoRepository.getAlbumDuration(changedAlbum.id)) {
                         if (first < changedAlbum.startDate) changedAlbum.startDate = first
                         if (second > changedAlbum.endDate) changedAlbum.endDate = second
