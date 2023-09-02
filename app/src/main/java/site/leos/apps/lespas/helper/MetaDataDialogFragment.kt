f9b93c12edfd49d08bb2d75703a412f94e143b9f
Jeff <scubajeffrey@criptext.com>
2023-09-02 14:05:05 +0800

    show map immediately if geo info is available in local DB

--- app/src/main/java/site/leos/apps/lespas/helper/MetaDataDialogFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/helper/MetaDataDialogFragment.kt
@@ -43,6 +43,7 @@ import androidx.preference.PreferenceManager
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -62,39 +63,44 @@ import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.time.format.TextStyle
 import java.util.*
+import java.util.concurrent.Executors
 import kotlin.math.roundToInt
 
 class MetaDataDialogFragment : LesPasDialogFragment(R.layout.fragment_info_dialog, 0.8f) {
     private var mapIntent = Intent(Intent.ACTION_VIEW)
     private lateinit var mapView: MapView
     private lateinit var mapButton: MaterialButton
-    private lateinit var photo: NCShareViewModel.RemotePhoto
+    private lateinit var localityTextView: TextView
+    private lateinit var remotePhoto: NCShareViewModel.RemotePhoto
     private lateinit var exifModel: ExifModel
     private val handler = Handler(Looper.getMainLooper())
     private lateinit var waitingMsg: Snackbar
+
+    private val mapDisplayThread = Executors.newFixedThreadPool(1).asCoroutineDispatcher()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //requireArguments().getParcelable<Photo>(KEY_MEDIA)?.let { photo = NCShareViewModel.RemotePhoto(it, "") }
         //requireArguments().getParcelable<NCShareViewModel.RemotePhoto>(KEY_REMOTE_MEDIA)?.let { photo = it }
-        requireArguments().parcelable<Photo>(KEY_MEDIA)?.let { photo = NCShareViewModel.RemotePhoto(it, "") }
-        requireArguments().parcelable<NCShareViewModel.RemotePhoto>(KEY_REMOTE_MEDIA)?.let { photo = it }
-        exifModel = ViewModelProvider(this, ExifModelFactory(requireActivity(), photo))[ExifModel::class.java]
+        requireArguments().parcelable<Photo>(KEY_MEDIA)?.let { remotePhoto = NCShareViewModel.RemotePhoto(it, "") }
+        requireArguments().parcelable<NCShareViewModel.RemotePhoto>(KEY_REMOTE_MEDIA)?.let { remotePhoto = it }
+        exifModel = ViewModelProvider(this, ExifModelFactory(requireActivity(), remotePhoto))[ExifModel::class.java]
     }
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<MaterialButton>(R.id.ok_button).setOnClickListener { dismiss() }
         mapButton = view.findViewById(R.id.map_button)
         mapView = view.findViewById(R.id.map)
+        localityTextView = view.findViewById(R.id.locality)
         // Don't abuse map tile source
         Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
 
         // Show basic information
-        view.findViewById<TextView>(R.id.info_filename).text = photo.photo.name.substringAfterLast("/")
-        view.findViewById<TextView>(R.id.info_shotat).text = String.format("%s %s", photo.photo.dateTaken.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()), photo.photo.dateTaken.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT, FormatStyle.MEDIUM)))
+        view.findViewById<TextView>(R.id.info_filename).text = remotePhoto.photo.name.substringAfterLast("/")
+        view.findViewById<TextView>(R.id.info_shotat).text = String.format("%s %s", remotePhoto.photo.dateTaken.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()), remotePhoto.photo.dateTaken.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT, FormatStyle.MEDIUM)))
+        if (remotePhoto.photo.latitude != Photo.NO_GPS_DATA) lifecycleScope.launch(mapDisplayThread) { showMap(remotePhoto.photo) }
 
         exifModel.getPhotoMeta().observe(viewLifecycleOwner) { photoMeta ->
             handler.removeCallbacksAndMessages(null)
@@ -132,76 +138,7 @@ class MetaDataDialogFragment : LesPasDialogFragment(R.layout.fragment_info_dialo
                 }
                 photoMeta.date?.let { view.findViewById<TextView>(R.id.info_shotat).text = String.format("%s %s", it.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()), it.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT, FormatStyle.MEDIUM))) }
 
-                try {
-                    if (latitude != Photo.NO_GPS_DATA) {
-                        with(mapView) {
-                            // Initialization
-                            setMultiTouchControls(true)
-                            setUseDataConnection(true)
-                            setTileSource(TileSourceFactory.MAPNIK)
-                            isFlingEnabled = false
-                            overlays.add(CopyrightOverlay(requireContext()))
-
-                            // Enable map panning inside Scrollview
-                            setOnTouchListener { v, event ->
-                                when (event.action) {
-                                    MotionEvent.ACTION_DOWN -> v.parent.parent.requestDisallowInterceptTouchEvent(true)  // TODO if layout xml changed, do make sure we get hold of the scrollview here
-                                    MotionEvent.ACTION_UP -> v.parent.parent.requestDisallowInterceptTouchEvent(false)
-                                }
-
-                                false
-                            }
-
-                            val poi = GeoPoint(latitude, longitude)
-                            controller.setZoom(18.5)
-                            controller.setCenter(poi)
-                            Marker(this).let {
-                                it.position = poi
-                                it.icon = ContextCompat.getDrawable(this.context, R.drawable.ic_baseline_location_marker_24)
-                                this.overlays.add(it)
-                            }
-                            if (this.context.resources.configuration.uiMode and UI_MODE_NIGHT_MASK == UI_MODE_NIGHT_YES) {
-                                overlayManager.tilesOverlay.setColorFilter(
-                                    ColorMatrixColorFilter(
-                                        floatArrayOf(
-                                            1.05f, 0f, 0f, 0f, -72f,  // red, reduce brightness about 1/4, increase contrast by 5%
-                                            0f, 1.05f, 0f, 0f, -72f,  // green, reduce brightness about 1/4, reduced contrast by 5%
-                                            0f, 0f, 1.05f, 0f, -72f,  // blue, reduce brightness about 1/4, reduced contrast by 5%
-                                            0f, 0f, 0f, 1f, 0f,
-                                        )
-                                    )
-                                )
-                            }
-                            invalidate()
-
-                            isVisible = true
-                        }
-
-                        mapIntent.data = Uri.parse(
-                            if (PreferenceManager.getDefaultSharedPreferences(requireContext()).getBoolean(getString(R.string.chinese_map_pref_key), false))
-                                Tools.wGS84ToGCJ02(doubleArrayOf(latitude, longitude)).let { "geo:${it[0]},${it[1]}?z=20" }
-                            else "geo:${latitude},${longitude}?z=20"
-                        )
-                        mapIntent.resolveActivity(requireActivity().packageManager)?.let {
-                            mapButton.apply {
-                                setOnClickListener {
-                                    startActivity(mapIntent)
-                                    dismiss()
-                                }
-
-                                isVisible = true
-                            }
-                        }
-                        // TODO use map text overlay instead
-                        if (locality.isNotEmpty() && country.isNotEmpty()) {
-                            view.findViewById<TextView>(R.id.locality).run {
-                                visibility = View.VISIBLE
-                                text = String.format("%s, %s", locality, country)
-                            }
-                        }
-                    }
-                } catch (_: Exception) {
-                }
+                if (latitude != Photo.NO_GPS_DATA && latitude != remotePhoto.photo.latitude && longitude != remotePhoto.photo.longitude) lifecycleScope.launch(mapDisplayThread) { showMap(photoMeta.photo) }
             }
         }
     }
@@ -229,6 +166,88 @@ class MetaDataDialogFragment : LesPasDialogFragment(R.layout.fragment_info_dialo
         super.onStop()
         handler.removeCallbacksAndMessages(null)
         if (waitingMsg.isShownOrQueued) waitingMsg.dismiss()
+    }
+
+    private var isMapInitialized = false
+    @SuppressLint("ClickableViewAccessibility")
+    private fun showMap(photo: Photo) {
+        try {
+            with(mapView) {
+                if (!isMapInitialized) {
+                    // Initialization
+                    setMultiTouchControls(true)
+                    setUseDataConnection(true)
+                    setTileSource(TileSourceFactory.MAPNIK)
+                    isFlingEnabled = false
+                    overlays.add(CopyrightOverlay(requireContext()))
+
+
+                    // Enable map panning inside Scrollview
+                    setOnTouchListener { v, event ->
+                        when (event.action) {
+                            // TODO if layout xml changed, do make sure we get hold of the scrollview here
+                            MotionEvent.ACTION_DOWN -> v.parent.parent.requestDisallowInterceptTouchEvent(true)
+                            MotionEvent.ACTION_UP -> v.parent.parent.requestDisallowInterceptTouchEvent(false)
+                        }
+                        false
+                    }
+
+                    isMapInitialized = true
+                }
+
+                // Show the map
+                val poi = GeoPoint(photo.latitude, photo.longitude)
+                controller.setZoom(18.5)
+                controller.setCenter(poi)
+                (if (overlays.last() is Marker) overlays.last() as Marker else Marker(this)).let {
+                    it.position = poi
+                    it.icon = ContextCompat.getDrawable(this.context, R.drawable.ic_baseline_location_marker_24)
+                    this.overlays.add(it)
+                }
+
+                overlays
+                if (this.context.resources.configuration.uiMode and UI_MODE_NIGHT_MASK == UI_MODE_NIGHT_YES) {
+                    overlayManager.tilesOverlay.setColorFilter(
+                        ColorMatrixColorFilter(
+                            floatArrayOf(
+                                1.05f, 0f, 0f, 0f, -72f,  // red, reduce brightness about 1/4, increase contrast by 5%
+                                0f, 1.05f, 0f, 0f, -72f,  // green, reduce brightness about 1/4, reduced contrast by 5%
+                                0f, 0f, 1.05f, 0f, -72f,  // blue, reduce brightness about 1/4, reduced contrast by 5%
+                                0f, 0f, 0f, 1f, 0f,
+                            )
+                        )
+                    )
+                }
+                invalidate()
+
+                isVisible = true
+            }
+
+            with(photo) {
+                mapIntent.data = Uri.parse(
+                    if (PreferenceManager.getDefaultSharedPreferences(requireContext()).getBoolean(getString(R.string.chinese_map_pref_key), false))
+                        Tools.wGS84ToGCJ02(doubleArrayOf(latitude, longitude)).let { "geo:${it[0]},${it[1]}?z=20" }
+                    else "geo:${latitude},${longitude}?z=20"
+                )
+                mapIntent.resolveActivity(requireActivity().packageManager)?.let {
+                    mapButton.apply {
+                        setOnClickListener {
+                            startActivity(mapIntent)
+                            dismiss()
+                        }
+
+                        isVisible = true
+                    }
+                }
+                // TODO use map text overlay instead
+                if (locality.isNotEmpty() && country.isNotEmpty()) {
+                    localityTextView.run {
+                        visibility = View.VISIBLE
+                        text = String.format("%s, %s", locality, country)
+                    }
+                }
+            }
+        } catch (_: Exception) {}
     }
 
     @Suppress("UNCHECKED_CAST")
