e4f6e3c202f4d92ebd890531bb6986be1d0c8c9b
Jeff <scubajeffrey@criptext.com>
2023-12-21 21:23:23 +0800

    fixed crash reported on Google Play Console

--- app/src/main/java/site/leos/apps/lespas/photo/PhotoSlideFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/photo/PhotoSlideFragment.kt
@@ -286,7 +286,7 @@ class PhotoSlideFragment : Fragment() {
                     super.onPageScrollStateChanged(state)
                     if (state == ViewPager2.SCROLL_STATE_SETTLING) handlerBottomControl.post(hideBottomControls)
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && state == ViewPager2.SCROLL_STATE_IDLE) slider.getChildAt(0)?.findViewById<View>(R.id.media)?.apply {
-                        window.colorMode = if (this is PhotoView && getTag(R.id.HDR_TAG) as Boolean) ActivityInfo.COLOR_MODE_HDR else ActivityInfo.COLOR_MODE_DEFAULT
+                        window.colorMode = if (this is PhotoView && getTag(R.id.HDR_TAG) as? Boolean == true) ActivityInfo.COLOR_MODE_HDR else ActivityInfo.COLOR_MODE_DEFAULT
                     }
                 }
 
