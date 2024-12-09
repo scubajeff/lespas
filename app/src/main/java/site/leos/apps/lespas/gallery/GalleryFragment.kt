57ee2659cbcc27dc61048d2a1a234e776aa46156
Jeff <scubajeffrey@criptext.com>
2024-12-09 20:29:44 +0800

    Revert "do not refresh archive if archive is not showing"
    This reverts commit b65ec4d0fe45ad867b63245a9537134105bade18.

--- app/src/main/java/site/leos/apps/lespas/gallery/GalleryFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/gallery/GalleryFragment.kt
@@ -693,10 +693,8 @@ class GalleryFragment: Fragment() {
             // Start observing archive snapshot file, which will be updated after new gallery image been backup to archive or files been removed from archive, snapshot file is updated by SyncAdapter after those actions finished successfully
             archiveSnapshotFileObserver = object : FileObserver("${localRoot}/${NCShareViewModel.ARCHIVE_SNAPSHOT_FILE}", CLOSE_WRITE) {
                 override fun onEvent(p0: Int, p1: String?) {
-                    if (_showArchive.value == ARCHIVE_ON) {
-                        _showArchive.value = REFRESHING_ARCHIVE
-                        viewModelScope.launch { imageModel.refreshArchive(false) }
-                    }
+                    if (_showArchive.value == ARCHIVE_ON) _showArchive.value = REFRESHING_ARCHIVE
+                    viewModelScope.launch { imageModel.refreshArchive(false) }
                 }
             }.apply { startWatching() }
         }
