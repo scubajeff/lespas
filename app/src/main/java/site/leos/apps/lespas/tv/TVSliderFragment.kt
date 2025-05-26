ffbeb46a3244bfa957b281cd259bb1396845b610
Jeff <scubajeffrey@criptext.com>
2025-05-26 11:10:33 +0800

    fixed browsing among video items

--- app/src/main/java/site/leos/apps/lespas/tv/TVSliderFragment.kt
+++ app/src/main/java/site/leos/apps/lespas/tv/TVSliderFragment.kt
@@ -17,7 +17,10 @@
 package site.leos.apps.lespas.tv
 
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
@@ -48,6 +51,7 @@ import site.leos.apps.lespas.helper.Tools
 import site.leos.apps.lespas.helper.Tools.parcelable
 import site.leos.apps.lespas.helper.VideoPlayerViewModel
 import site.leos.apps.lespas.helper.VideoPlayerViewModelFactory
+import site.leos.apps.lespas.photo.Photo
 import site.leos.apps.lespas.publication.NCShareViewModel
 import site.leos.apps.lespas.publication.RemoteMediaFragment.PhotoDiffCallback
 
@@ -59,6 +63,8 @@ class TVSliderFragment: Fragment() {
     private val imageLoaderModel: NCShareViewModel by activityViewModels()
     private lateinit var playerViewModel: VideoPlayerViewModel
 
+    private val handler = Handler(Looper.getMainLooper())
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -73,6 +79,11 @@ class TVSliderFragment: Fragment() {
             { media, imageView, type -> if (type != NCShareViewModel.TYPE_NULL) imageLoaderModel.setImagePhoto(media, imageView!!, NCShareViewModel.TYPE_FULL) },
             { media, imageView, plManager, panorama -> imageLoaderModel.setImagePhoto(media, imageView!!, NCShareViewModel.TYPE_PANORAMA, plManager, panorama) },
             { view -> imageLoaderModel.cancelSetImagePhoto(view) },
+            { delta ->
+                slider.beginFakeDrag()
+                slider.fakeDragBy(delta * slider.width)
+                slider.endFakeDrag()
+            }
         )
 
         var isShared = false
@@ -113,9 +124,23 @@ class TVSliderFragment: Fragment() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        mediaAdapter.setPauseVideo(true)
+    }
+
+    override fun onStop() {
+        try { if (mediaAdapter.getPhotoAt(slider.currentItem).mimeType.startsWith("video")) handler.postDelayed({ playerViewModel.pause(Uri.EMPTY) }, 300) } catch (_: IndexOutOfBoundsException) {}
+
+        super.onStop()
+    }
+
     class RemoteMediaAdapter(context: Context, displayWidth: Int, private val basePath: String, playerViewModel: VideoPlayerViewModel,
-        clickListener: (Boolean?) -> Unit, imageLoader: (NCShareViewModel.RemotePhoto, ImageView?, type: String) -> Unit, panoLoader: (NCShareViewModel.RemotePhoto, ImageView?, PLManager, PLSphericalPanorama) -> Unit, cancelLoader: (View) -> Unit
+        clickListener: (Boolean?) -> Unit, imageLoader: (NCShareViewModel.RemotePhoto, ImageView?, type: String) -> Unit, panoLoader: (NCShareViewModel.RemotePhoto, ImageView?, PLManager, PLSphericalPanorama) -> Unit, cancelLoader: (View) -> Unit,
+        private val scrollListener: (Float) -> Unit,
     ): SeamlessMediaSliderAdapter<NCShareViewModel.RemotePhoto>(context, displayWidth, PhotoDiffCallback(), playerViewModel, clickListener, imageLoader, panoLoader, cancelLoader) {
+        fun getPhotoAt(position: Int): Photo = currentList[position].photo
+
         override fun getVideoItem(position: Int): VideoItem = with(getItem(position) as NCShareViewModel.RemotePhoto) { VideoItem("$basePath$remotePath/${photo.name}".toUri(), photo.mimeType, photo.width, photo.height, photo.id) }
         override fun getItemTransitionName(position: Int): String = (getItem(position) as NCShareViewModel.RemotePhoto).photo.id
         override fun getItemMimeType(position: Int): String = (getItem(position) as NCShareViewModel.RemotePhoto).photo.mimeType
@@ -129,7 +154,7 @@ class TVSliderFragment: Fragment() {
                     if (event?.action == KeyEvent.ACTION_DOWN) {
                         when(keyCode) {
                             KeyEvent.KEYCODE_DPAD_LEFT, KeyEvent.KEYCODE_BUTTON_L1, KeyEvent.KEYCODE_DPAD_RIGHT, KeyEvent.KEYCODE_BUTTON_R1 -> {
-                                recyclerView.smoothScrollToPosition((lm.findFirstVisibleItemPosition() + if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT || keyCode == KeyEvent.KEYCODE_BUTTON_L1) -1 else 1).coerceIn(0, recyclerView.adapter!!.itemCount))
+                                scrollListener(if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT || keyCode == KeyEvent.KEYCODE_BUTTON_L1) 1f else -1f)
                                 return true
                             }
                             KeyEvent.KEYCODE_DPAD_CENTER, KeyEvent.KEYCODE_ENTER, KeyEvent.KEYCODE_NUMPAD_ENTER, KeyEvent.KEYCODE_BUTTON_SELECT, KeyEvent.KEYCODE_BUTTON_A -> {
